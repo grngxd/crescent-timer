b82a4b7aad5a55913d74a513ddbd9d270b3daaf4
grng <36968271+grngxd@users.noreply.github.com>
2024-03-25 19:42:29 +0000

    stack change (QTPS)

--- components/Sidebar/SidebarButton.tsx
+++
@@ -1,7 +0,0 @@
-export default function SidebarButton(props : any) {
-    return (
-           <div className="bg-x-500 hover:bg-slate-700 active:bg-slate-500 active:translate-y-1 hover:scale-105 transition-all duration-100 p-2 rounded-lg">
-                {props.icon}
-           </div> 
-    )
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {
-      colors: {
-        x: {
-          300: "#b8b9bb",
-          400: "#12151e",
-          500: "#0B0E17"
-        }
-      }
-    },
+    extend: {},
   },
-  plugins: [
-    require('tailwind-scrollbar'),
-  ],
-}
+  plugins: [],
+};
