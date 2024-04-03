a8b2e211813b5585e9ef73f7ab407b8baf4081e5
grng <36968271+grngxd@users.noreply.github.com>
2024-04-03 15:44:23 +0100

    add stats/profile routes and update nav

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { browser } from "$app/environment";
+  import { goto } from "$app/navigation";
   import { theme } from "$lib/stores/theme";
   import { css } from "@emotion/css";
   import GraphDownLineDuotone from "./icons/solar/GraphDownLineDuotone.svelte";
@@ -10,27 +12,33 @@
     color: $theme.colors.text.primary
 })}`}>
 <button class={`text-3xl md:text-3xl font-bold flex w-full h-full justify-center items-center transition-all duration-200 ${css({
-    backgroundColor: `${$theme.colors.text.tertiary}05`,
+    backgroundColor: browser ? (window.location.pathname === "/" ? `${$theme.colors.text.tertiary}11` : `${$theme.colors.text.tertiary}05`) : "",
     ":hover": {
-        backgroundColor: `${$theme.colors.text.tertiary}11`
+        backgroundColor: browser ? (window.location.pathname === "/" ? `${$theme.colors.text.tertiary}16` : `${$theme.colors.text.tertiary}11`) : ""
     }
-})}`}>
+})}`} on:click={() => {
+    goto("/")
+}}>
     <StopwatchLinear />
 </button>
 <button class={`text-[4rem] md:text-3xl font-bold flex w-full h-full justify-center items-center transition-all duration-200 ${css({
-    backgroundColor: `${$theme.colors.text.tertiary}05`,
+    backgroundColor: browser ? (window.location.pathname === "/stats" ? `${$theme.colors.text.tertiary}11` : `${$theme.colors.text.tertiary}05`) : "",
     ":hover": {
-        backgroundColor: `${$theme.colors.text.tertiary}11`
+        backgroundColor: browser ? (window.location.pathname === "/stats" ? `${$theme.colors.text.tertiary}16` : `${$theme.colors.text.tertiary}11`) : ""
     }
-})}`}>
+})}`} on:click={() => {
+    goto("/stats")
+}}>
     <GraphDownLineDuotone />
 </button>
 <button class={`text-3xl md:text-3xl font-bold flex w-full h-full justify-center items-center transition-all duration-200 ${css({
-    backgroundColor: `${$theme.colors.text.tertiary}05`,
+    backgroundColor: browser ? (window.location.pathname === "/profiles" ? `${$theme.colors.text.tertiary}11` : `${$theme.colors.text.tertiary}05`) : "",
     ":hover": {
-        backgroundColor: `${$theme.colors.text.tertiary}11`
+        backgroundColor: browser ? (window.location.pathname === "/profiles" ? `${$theme.colors.text.tertiary}16` : `${$theme.colors.text.tertiary}11`) : ""
     }
-})}`}>
+})}`} on:click={() => {
+    goto("/profiles")
+}}>
     <UserCircleLinear />
 </button>
 </div>
---
+++ src/routes/profiles/[profile]/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import { page } from '$app/stores';
+  const id = $page.params.profile;
+  // TODO: Fetch profile data
+</script>
