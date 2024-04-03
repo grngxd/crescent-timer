9b45a2ef3335220309b7353fe1e4a6f41f607cbb
grng <36968271+grngxd@users.noreply.github.com>
2024-04-03 13:15:42 +0100

    arrow keys

--- src/components/Scramble.svelte
+++ src/components/Scramble.svelte
@@ -32,14 +32,34 @@
     scrambles.update(s => [...s, newScramble]);
     currentIndex++;
   }
+
+  function handleKeyUp(event: KeyboardEvent) {
+    if (event.key === "ArrowLeft" || event.key === "ArrowDown") {
+      event.preventDefault();
+      previousScramble();
+      return;
+    } else if (event.key === "ArrowRight" || event.key === "ArrowUp") {
+      event.preventDefault();
+      newScramble();
+      return;
+    }
+  }
+
+  onMount(() => {
+    window.addEventListener("keyup", handleKeyUp);
+    return () => window.removeEventListener("keyup", handleKeyUp);
+  });
 </script>
 
 <div class={`z-[2] flex flex-row py-4 px-8 md:px-32 justify-center gap-16 items-center flex-grow-0 backdrop-blur-lg ${css({
   backgroundColor: `${$theme.background}22`,
 })}`}>
-  <button class={`font-space-grotesk font-light text-2xl ${
+  <button class={`font-space-grotesk font-light text-2xl rounded-full p-2 duration-150 transition-all active:scale-125 ${
     css({
-      color: $theme.colors.text.primary
+      color: $theme.colors.text.primary,
+      ":hover": {
+        backgroundColor: `${$theme.colors.text.secondary}55`
+      }
     })
   }`} on:click={previousScramble}><ArrowLongLeft /></button>
 
@@ -47,9 +67,12 @@
     color: $theme.colors.text.primary
   })}`}>{$scramble || "Loading Scramble..."}</p>
 
-  <button class={`font-space-grotesk font-light text-2xl ${
+  <button class={`font-space-grotesk font-light text-2xl rounded-full p-2 duration-200 transition-all active:scale-125 ${
     css({
-      color: $theme.colors.text.primary
+      color: $theme.colors.text.primary,
+      ":hover": {
+        backgroundColor: `${$theme.colors.text.secondary}55`
+      }
     })
   }`} on:click={newScramble}><ArrowLongRight /></button>
 </div>
