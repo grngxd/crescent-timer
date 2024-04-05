b46d67646627f1522a2710fd416e887f0fe851ef
grng <36968271+grngxd@users.noreply.github.com>
2024-04-05 12:18:35 +0100

    update mobile navbar

--- src/components/Tabs.svelte
+++ src/components/Tabs.svelte
@@ -3,7 +3,10 @@
   import { goto } from "$app/navigation";
   import { page } from "$app/stores";
   import { theme } from "$lib/stores/theme";
+  import { timing } from "$lib/stores/timer";
   import { css } from "@emotion/css";
+  import { quintInOut } from "svelte/easing";
+  import { slide } from "svelte/transition";
   import GraphDownLineDuotone from "./icons/solar/GraphDownLineDuotone.svelte";
   import StopwatchLinear from "./icons/solar/StopwatchLinear.svelte";
   import UserCircleLinear from "./icons/solar/UserCircleLinear.svelte";
@@ -11,39 +14,43 @@
   let path: string;
   $: path = $page.url.pathname;
 </script>
-
-<div class={`text-center z-[2] flex-row md:px-32 justify-center items-center flex-[0.1] backdrop-blur-lg flex md:hidden ${css({
+{#if !$timing}
+<div class={`absolute bottom-0 w-screen z-[2] h-[10%] backdrop-blur-lg inline-flex md:hidden ${css({
     backgroundColor: `${$theme.background}22`,
     color: $theme.colors.text.primary
-})}`}>
-<button class={`text-3xl md:text-3xl font-bold flex w-full h-full justify-center items-center transition-all duration-200 ${css({
-    backgroundColor: browser ? (path === "/" ? `${$theme.colors.text.tertiary}11` : `${$theme.colors.text.tertiary}05`) : "",
-    ":hover": {
-        backgroundColor: browser ? (path === "/" ? `${$theme.colors.text.tertiary}16` : `${$theme.colors.text.tertiary}11`) : ""
-    }
-})}`} on:click={() => {
-    goto("/")
-}}>
-    <StopwatchLinear />
-</button>
-<button class={`text-[4rem] md:text-3xl font-bold flex w-full h-full justify-center items-center transition-all duration-200 ${css({
-    backgroundColor: browser ? (path === "/stats" ? `${$theme.colors.text.tertiary}11` : `${$theme.colors.text.tertiary}05`) : "",
-    ":hover": {
-        backgroundColor: browser ? (path === "/stats" ? `${$theme.colors.text.tertiary}16` : `${$theme.colors.text.tertiary}11`) : ""
-    }
-})}`} on:click={() => {
-    goto("/stats")
-}}>
-    <GraphDownLineDuotone />
-</button>
-<button class={`text-3xl md:text-3xl font-bold flex w-full h-full justify-center items-center transition-all duration-200 ${css({
-    backgroundColor: browser ? (path === "/profiles" ? `${$theme.colors.text.tertiary}11` : `${$theme.colors.text.tertiary}05`) : "",
-    ":hover": {
-        backgroundColor: browser ? (path === "/profiles" ? `${$theme.colors.text.tertiary}16` : `${$theme.colors.text.tertiary}11`) : ""
-    }
-})}`} on:click={() => {
-    goto("/profiles")
-}}>
-    <UserCircleLinear />
-</button>
+})}`}
+transition:slide={{duration:250, axis: "y", delay: 0, easing: quintInOut}}>
+    <button class={`text-3xl md:text-3xl font-bold flex w-full h-full justify-center items-center transition-all duration-200 ${css({
+        backgroundColor: browser ? (path === "/" ? `${$theme.colors.text.tertiary}11` : `${$theme.colors.text.tertiary}05`) : "",
+        ":hover": {
+            backgroundColor: browser ? (path === "/" ? `${$theme.colors.text.tertiary}16` : `${$theme.colors.text.tertiary}11`) : ""
+        }
+    })}`} on:click={() => {
+        goto("/")
+    }}>
+        <StopwatchLinear />
+    </button>
+
+    <button class={`text-[4rem] md:text-3xl font-bold flex w-full h-full justify-center items-center transition-all duration-200 ${css({
+        backgroundColor: browser ? (path === "/stats" ? `${$theme.colors.text.tertiary}11` : `${$theme.colors.text.tertiary}05`) : "",
+        ":hover": {
+            backgroundColor: browser ? (path === "/stats" ? `${$theme.colors.text.tertiary}16` : `${$theme.colors.text.tertiary}11`) : ""
+        }
+    })}`} on:click={() => {
+        goto("/stats")
+    }}>
+        <GraphDownLineDuotone />
+    </button>
+
+    <button class={`text-3xl md:text-3xl font-bold flex w-full h-full justify-center items-center transition-all duration-200 ${css({
+        backgroundColor: browser ? (path === "/profiles" ? `${$theme.colors.text.tertiary}11` : `${$theme.colors.text.tertiary}05`) : "",
+        ":hover": {
+            backgroundColor: browser ? (path === "/profiles" ? `${$theme.colors.text.tertiary}16` : `${$theme.colors.text.tertiary}11`) : ""
+        }
+    })}`} on:click={() => {
+        goto("/profiles")
+    }}>
+        <UserCircleLinear />
+    </button>
 </div>
+{/if}
