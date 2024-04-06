dfeeaf4d5a38a8c2b2be3de614f4ed71dbc86409
grng <36968271+grngxd@users.noreply.github.com>
2024-04-06 10:58:41 +0100

    A

--- src/components/SessionManager.svelte
+++ src/components/SessionManager.svelte
@@ -42,7 +42,7 @@
 
 <!-- Dropdown Menu -->
 {#if open}
-    <div class={`absolute top-16 right-6 w-64 max-h-48 py-2 px-2 bg-white rounded-lg shadow-lg z-3 overflow-x-clip overflow-y-auto scrollbar scrollbar-thin ${css({
+    <div class={`absolute hidden lg:flex top-16 right-6 w-64 max-h-48 py-2 px-2 bg-white rounded-lg shadow-lg z-3 overflow-x-clip overflow-y-auto scrollbar scrollbar-thin ${css({
         color: $theme.colors.text.primary,
         background: `${$theme.colors.text.tertiary}22`,
         scrollbarColor: `${$theme.colors.text.tertiary}55 ${$theme.colors.text.tertiary}22`,
